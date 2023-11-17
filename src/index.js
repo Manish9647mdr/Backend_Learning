6fd8a5911baabf392ce273e6369f34068ff08837
Manandhar Manish <m_manish@la.jobtkk.co.jp>
2023-11-17 18:26:54 +0900

    util code create and update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ dotenv.config({
       path : './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+      app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`);
+      })
+})
+.catch((err) => {
+      console.log("MONGO db connection failed !!", err);
+})
 
 
 
@@ -33,4 +41,7 @@ try {
      throw error
 }
 })()
+
+PORT=8000
+MONGODB_URI=mongodb+srv://manish2022job:Password@cluster0.hhg6gvr.mongodb.net
 */
